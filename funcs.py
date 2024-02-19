6a06c2f492e2d714e7a4c2dfbc89eba2987a1ab8
Glushkov Alex <avgl@mail.ru>
2024-02-19 20:13:27 +0300

    substraction function

--- funcs.py
+++ funcs.py
@@ -2,4 +2,9 @@
 def additional_func(a,b):
     """функция, которая складывает два числа"""
 
-    return a + b
+    return (a + b)
+
+def substarction_func(c,d):
+    """функция, которая вычитает два числа"""
+
+    return (c - d)
